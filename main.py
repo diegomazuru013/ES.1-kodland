cf41718d837da98d73113ede4dea952f969e1e98
diegomazuru013 <diegomazuru@gmail.com>
2026-07-17 17:46:02 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Qualcosa di eccezionalmente strano o imbarazzante",
+            "LOL": "Una risposta comune a qualcosa di divertente",
+            }
+
+parola = input("Scrivi una parola che non capisci (usa solo lettere maiuscole!): ")
+
+
+if parola in meme_dict.keys():
+    # Cosa fare se la parola è stata trovata?
+    print(meme_dict[parola])
+else:
+    # Cosa fare se la parola non è stata trovata?
+    print("non é stato trovato")
